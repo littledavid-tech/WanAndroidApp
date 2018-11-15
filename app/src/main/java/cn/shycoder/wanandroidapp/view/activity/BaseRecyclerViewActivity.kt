bf89efdf3e40a71eae3264c58d21ee7cdf49116d
LD <wangyidong_cn@outlook.com>
2018-11-15 16:51:57 +0800

    修复查看知识体系文章的时候 Toolbar不折叠的Bug

--- app/src/main/java/cn/shycoder/wanandroidapp/view/activity/BaseRecyclerViewActivity.kt
+++ app/src/main/java/cn/shycoder/wanandroidapp/view/activity/BaseRecyclerViewActivity.kt
@@ -3,12 +3,13 @@ package cn.shycoder.wanandroidapp.view.activity
 import cn.shycoder.wanandroidapp.R
 import cn.shycoder.wanandroidapp.presenter.contract.BaseRecyclerViewContract
 import cn.shycoder.wanandroidapp.view.BaseActivity
+import cn.shycoder.wanandroidapp.view.BaseToolBarActivity
 
 /**
  * Created by ShyCoder on 11/15/2018.
  */
-class BaseRecyclerViewActivity<T : BaseRecyclerViewContract.Presenter<*>>
-    : BaseActivity<T>(), BaseRecyclerViewContract.View {
+abstract class BaseRecyclerViewActivity<T : BaseRecyclerViewContract.Presenter<*>>
+    : BaseToolBarActivity<T>(), BaseRecyclerViewContract.View {
 
     override fun enableLoadMore(boolean: Boolean) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
